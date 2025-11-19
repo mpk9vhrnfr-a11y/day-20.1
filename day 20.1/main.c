9941764c9258c52fee5866ea51c985d7887a2263
sanaa <Sanaakumar07@gmail.com>
2025-11-19 20:53:58 +0530

    c

--- day 20.1/main.c
+++ day 20.1/main.c
@@ -1,15 +1,26 @@
-//
-//  main.c
-//  day 20.1
-//
-//  Created by Sanaa Kumar on 19/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int n, num, first, last, x = 1, temp;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    num = n;
+    last = num % 10;
+
+    while(num >= 10) {
+        num = num / 10;
+        x = x * 10;
+    }
+    first = num;
+
+    temp = n % x;
+    temp = temp / 10;
+
+    n = last * x+ temp * 10 + first;
+
+    printf("Number after swapping first and last digit = %d", n);
+
+    return 0;
 }
